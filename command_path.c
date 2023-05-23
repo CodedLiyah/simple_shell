f4191927265a08c72413ebd307adb5ddff180e57
CodedLiyah <remote.liyah@gmail.com>
2023-05-23 13:04:04 -0700

    Passed

--- command_path.c
+++ command_path.c
@@ -1,88 +1,34 @@
 #include "shell.h"
 
 /**
- * _myenv - prints current environment
- * @info: Structure used to maintain constant function prototype.
- *
- * Return : Always 0 (Success)
+ * _getenv - Prints global variable
+ * @name: Name of variable
+ * Return: String value
  */
-int _myenv(info_t *info)
+char *_getenv(const char *name)
 {
-	print_list_str(info->env);
-	return (0);
-}
+	int i, j;
+	char *value;
 
-/**
- * _getenv - prints value of an environ variable
- * @name: env var name
- * @info: Structure used to maintain constant function prototype.
- *
- * Return: Always 0(Success)
- */
-char *_getenv(info_t *info, const char *name)
-{
-	list_t *node = info->env;
-	char *j;
-
-	while (node)
+	if(!name)
+		return(NULL);
+	for (i = 0; environ[i]; i++)
 	{
-		j = starts_with(node->str, name);
-		if (j && *j)
-			return (j);
-		node = node->next;
+		j = 0;
+		if (name[j] == environ[i][j])
+		{
+			while (name[j])
+			{
+				if (name[j] != environ[i][j])
+					break;
+				j++;
+			}
+			if (name[j] == '\0')
+			{
+				value = (environ[i] + j + 1);
+				return (value);
+			}
+		}
 	}
-	return (NULL);
-}
-
-/**
- * _newenv- Begin new environment variable
- * @info: Structure used to maintain constant function prototype.
- * Return: Always 0 (Success) 
- */
-int _newenv(info_t *info)
-{
-	if (info->argc != 3)
-	{
-		_eputs("Invalid number of arguments\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
-}
-
-/**
- * _rmovenv- Removes environment variable
- * @info: Structure used to maintain constant function prototype.
- * Return : Always 0 (Success) 
- */
-int _rmovenv(info_t *info)
-{
-	int a;
-
-	if (info->arg == 1)
-	{
-		_eputs("Invalid number of arguments\n");
-		return (1);
-	}
-	for (a = 1; a <= info->argc; a++)
-		_unsetenv(info, info->argv[a]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populates env linked list
- * @info: Structure used to maintain constant function prototype.
- * Return: Always 0 (Success)
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t a;
-
-	for (a = 0; environ[a]; a++)
-		add_node_end(&node, environ[a], 0);
-	info->env = node;
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,65 +1,62 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
 #include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <signal.h>
 
-typedef struct(info_s * info_t);
-/**Add the required members used in shell_utils.c**/
-int new_interactive(info_t *info); /**Example member interactive function**/
-int _myhistory(info_t *info); /**Example member  display_history function**/
-/**Add any other members you need in other functions**/
-
-/** convert_String.c **/
-int new_interactive(info_t *info);
-int check_delim(char dc, char *delim);
-int is_custom_alpha(int ca);
-int convert_string(char *sc);
-
-/** command_handlers.c **/
-int shell_exit(info_t *info);
-
-
-/** shell_utils.c **/
-int display_history(info_t *info);
-int unset_alias(info_t *info, char *str);
-int set_alias(info_t *info, char *str);
-int print_alias(list_t *node);
-int handle_alias(info_t *info);
-
-/** command_path **/
-int _myenv(info_t *info);
-char *_getenv (info_t *info, const char *name);
-int _newenv(info_t *info);
-int _rmovenv(info_t *info);
-int _populate_env_list(info_t *info);
-
-
-/** errors.c **/
-void _eputs(char *);
-int _eputchar(char);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
-
-/** errors1.c **/
-int _erratoi(char *);
-void print_error(info_t *, char *);
-int print_d(int, int);
-char *convert_number(long int, int, int);
-void remove_comments(char *);
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *concat_all(char *name, char *sep, char *value);
+char **splitsring(char *str, const char *delim);
+void execute(char **argv);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 extern char **environ;
 
+/**
+ * struct list_path - List containing PATH directories
+ * @dir: directory in PATH
+ * @p: pointer to next node
+ */
+typedef struct list_path
+{
+	char *dir;
+	struct list_path *p;
+} list_path;
+
+char *_getenv(const char *name);
+list_path *add_node_end(list_path **head, char *str);
+list_path *linkpath(char *path);
+char *_which(char *filename, list_path *head);
+
+/**
+ * struct mybuild - Pointer to function with corresponding building command
+ * @name: buildin command
+ * @func: execute the buildin command
+ */
+typedef struct mybuild
+{
+	char *name;
+	void (*func)(char **);
+} mybuild;
+
+void(*checkbuild(char **arv))(char **arv);
+int _atoi(char *s);
+void exitt(char **arv);
+void env(char **arv);
+void _setenv(char **arv);
+void _unsetenv(char ** arv);
+void freearv(char **arv);
+void free_list(list_path *head);
+
 #endif
+
