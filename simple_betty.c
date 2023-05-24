12054c0d96d2229b794ff6d913cc90846ea7dfb8
CodedLiyah <remote.liyah@gmail.com>
2023-05-24 16:17:40 -0700

    Simple Betty

--- simple_betty.c
+++ simple_betty.c
@@ -1,16 +1,28 @@
 #include "shell.h"
 
 /**
- * the_alpha - Prints the alphabet
- * @i: The character to input
- *
- * Return:1 on success, 0 otherwise
- **/
-
-int the_alpha(int i)
+ * _atoi - converts a string into an integer
+ *@s: pointer to a string
+ *Return: the integer
+ */
+int _atoi(char *s)
 {
-	if ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
-		return (1);
-	else
-		return (0);
+	int i, integer, sign = 1;
+
+	i = 0;
+	integer = 0;
+	while (!((s[i] >= '0') && (s[i] <= '9')) && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * (-1);
+		}
+		i++;
+	}
+	while ((s[i] >= '0') && (s[i] <= '9'))
+	{
+		integer = (integer * 10) + (sign * (s[i] - '0'));
+		i++;
+	}
+	return (integer);
 }
