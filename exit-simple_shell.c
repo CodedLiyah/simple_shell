77cf6dec5df6c4827c27e649acae3f8d67c01e17
CodedLiyah <remote.liyah@gmail.com>
2023-05-24 14:58:06 -0700

    Check environ

--- exit-simple_shell.c
+++ exit-simple_shell.c
@@ -1,74 +1,30 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string to another
- *@loca: the location  string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ * _myexit - exits the shell
+ * @list: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: exits with a given exit status
+ * (0) if info.argv[0] != "exit"
  */
-char *_strncpy(char *loca, char *src, int n)
+int _myexit(info_t *info)
 {
-	int a, j;
-	char *s = loca;
+	int exitcheck;
 
-	a = 0;
-	while (src[a] != '\0' && a < n - 1)
+	if (info_t->argv[1]) /* If there is an exit arguement */
 	{
-		loca[a] = src[a];
-		a++;
-	}
-	if (a < n)
-	{
-		j = a;
-		while (j < n)
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
 		{
-			loca[j] = '\0';
-			j++;
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			puts(info->argv[1]);
+			putchar('\n');
+			return (1);
 		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
 	}
-	return (s);
-}
-
-/**
- **_strncat - links two strings
- *@loca: This is pointer to the destination array
- *@src: string to appended
- *@n: This is the maximum number of characters to be appended.
- *Return: returns a pointer to the resulting string destination
- */
-char *_strncat(char *loca, char *src, int n)
-{
-	int a, j;
-	char *s = loca;
-
-	a = 0;
-	j = 0;
-	while (loca[a] != '\0')
-		a++;
-	while (src[j] != '\0' && j < n)
-	{
-		loca[a] = src[j];
-		a++;
-		j++;
-	}
-	if (j < n)
-		loca[a] = '\0';
-	return (s);
-}
-
-/**
- **_strchr - searches for the first occurrence of the character
- *@str: the string to be scanned
- *@c: the character to be searched for
- *Return: first character, or NULL if the character is not found.
- */
-char *_strchr(char *str, char c)
-{
-	do {
-		if (*str == c)
-			return (str);
-	} while (*str++ != '\0');
-
-	return (NULL);
+	info->err_num = -1;
+	return (-2);
 }
