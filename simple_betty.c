15a92b383d9c63efe4bf6f4fa971f7d85ba590c6
CodedLiyah <remote.liyah@gmail.com>
2023-05-24 01:07:47 -0700

    Simple_betty

--- simple_betty.c
+++ simple_betty.c
@@ -1,4 +1,16 @@
 #include "shell.h"
 
 /**
+ * _isalpha - Prints the alphabet
+ * @i: The character to input
  *
+ * Return:1 if i is alphabetic, 0 otherwise
+ **/
+
+int _isalpha(int i)
+{
+	if ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
