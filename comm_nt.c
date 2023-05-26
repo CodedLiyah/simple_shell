6f2aa07ad3d4f00b244345143e97cd82b924e952
Shaylee21 <shayleepillay021@gmail.com>
2023-05-26 03:58:11 -0700

    Update

--- comm_nt.c
+++ comm_nt.c
@@ -1,75 +1,97 @@
 #include "shell.h"
 
 /**
- * semicolon - search if in the input there is a ";" or a ";;"
- * @line: complete input of user
- * @loop: number of actual loop
- * @argv: array of argument string
- * Return: 1 if find ";" or ";;" or 0 if not
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
  */
-int semicolon(char *line, int loop, char **argv)
+int _myexit(info_t *info)
 {
-	int valid = 0, cont = 0;
+	int exitcheck;
 
-	while (line[cont] != '\0')
+	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		if (line[0] == ';')
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
 		{
-			valid = 1;
-			write(STDERR_FILENO, argv[0], _strlen(argv[0]));
-			write(STDERR_FILENO, ": ", 2);
-			print_number(loop);
-			write(STDERR_FILENO, ": ", 2);
-			write(STDERR_FILENO, "Syntax error: ", 14);
-			write(STDERR_FILENO, ";", 1);
-			write(STDERR_FILENO, " unexpected\n", 12);
-			break;
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
 		}
-		if (line[cont] == ';' && line[cont + 1] == ';')
-		{
-			valid = 1;
-			write(STDERR_FILENO, argv[0], _strlen(argv[0]));
-			write(STDERR_FILENO, ": ", 2);
-			print_number(loop);
-			write(STDERR_FILENO, ": ", 2);
-			write(STDERR_FILENO, "Syntax error: ", 14);
-			write(STDERR_FILENO, ";;", 2);
-			write(STDERR_FILENO, " unexpected\n", 12);
-			break;
-		}
-		cont++;
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
 	}
-	return (valid);
+	info->err_num = -1;
+	return (-2);
 }
-/**
- *_comments - remove commentaries
- *@line: input of user
- * Return: the new input
- */
-char *_comments(char *line)
-{
-	int a = 0, c = 0, flag = 0;
 
-	while (line[c] != '\0')
-		c++;
-	while (line[a] != '\0')
+/**
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
 	{
-		if (line[0] == '#')
-		{
-			flag = 1;
-			break;
-		}
-		if (line[a] == '#' && line[a - 1] == ' ')
-		{
-			flag = 1;
-			break;
-		}
-		a++;
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
 	}
-	if (flag == 1)
+	else if (_strcmp(info->argv[1], "-") == 0)
 	{
-		for (; a < c; a++)
-			line[a] = 0;
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
-	return (line);
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
 }
